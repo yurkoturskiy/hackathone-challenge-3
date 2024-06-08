b94a641a1e9de0a80b9412b919a76318bfc4d27c
Yurko Turskiy <guandjoy@gmail.com>
2024-06-08 15:29:23 -0700

    cleanup

--- next-app/src/components/StepOne.tsx
+++ next-app/src/components/StepOne.tsx
@@ -4,13 +4,11 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
 
 export default function StepOne({
   setStep,
@@ -19,33 +17,33 @@ export default function StepOne({
 }: StepProps) {
   return (
     <Card className="max-w-[800px] mx-auto space-y-4">
-      <CardHeader className="slace-y-4">
+      <CardHeader className="slace-y-4 pb-0">
         <CardTitle>Generate a highlight reel</CardTitle>
         <CardDescription>
           Use YouTube videos to create a highlight reel of your favorite
           moments.
         </CardDescription>
       </CardHeader>
-      <CardContent className="space-y-4">
+      <CardContent className="space-y-4 mt-0">
         <div>
           <Label>YouTube link</Label>
-          <Input
-            placeholder="https://youtube.com/watch?your-video"
-            onChange={(e) => {
-              setVideoURL(e.target.value);
-            }}
-          />
+          <div className="flex gap-2">
+            <Input
+              placeholder="https://youtube.com/watch?your-video"
+              onChange={(e) => {
+                setVideoURL(e.target.value);
+              }}
+            />
+            <Button
+              onClick={() => {
+                setStep((step) => step + 1);
+              }}
+            >
+              Continue
+            </Button>
+          </div>
         </div>
       </CardContent>
-      <CardFooter>
-        <Button
-          onClick={() => {
-            setStep((step) => step + 1);
-          }}
-        >
-          Continue
-        </Button>
-      </CardFooter>
     </Card>
   );
 }
--- next-app/src/components/StepTwo.tsx
+++ next-app/src/components/StepTwo.tsx
@@ -1,16 +1,12 @@
 import { StepProps } from "@/app/page";
-import Iframe from "react-iframe";
 import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import YouTubeEmbed from "./YouTubeEmbeded";
 
