9657c9b2d073729b322b6cf6a92bc07bf01b806c
Yurko Turskiy <guandjoy@gmail.com>
2024-06-08 13:50:56 -0700

    setup card

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,22 +1,38 @@
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="max-w-[800px] mx-auto space-y-4">
-      <div>
-        <Label>YouTube link</Label>
-        <Input placeholder="https://youtube.com/watch?your-video" />
-      </div>
-      <div>
-        <Label>Promp</Label>
-        <Textarea placeholder="Create a highlight reel of Roger Federer discussing his retirement" />
-      </div>
+    <Card className="max-w-[800px] mx-auto space-y-4">
+      <CardHeader className="slace-y-4">
+        <CardTitle>Generate a highlight reel</CardTitle>
+        <CardDescription>
+          Use YouTube videos to create a highlight reel of your favorite
+          moments.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <div>
+          <Label>YouTube link</Label>
+          <Input placeholder="https://youtube.com/watch?your-video" />
+        </div>
+        <div>
+          <Label>Promp</Label>
+          <Textarea placeholder="Create a highlight reel of Roger Federer discussing his retirement" />
+        </div>
 
-      <Button>Generate</Button>
-    </div>
+        <Button>Generate</Button>
+      </CardContent>
+    </Card>
   );
 }
