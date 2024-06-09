55c3352d64e2406075521a4a273f83af6f5878cf
Yurko Turskiy <guandjoy@gmail.com>
2024-06-08 18:16:37 -0700

    add link to card in indexes page

--- next-app/src/app/indexes/page.tsx
+++ next-app/src/app/indexes/page.tsx
@@ -4,10 +4,12 @@ import YouTubeEmbed from "@/components/YouTubeEmbeded";
 import { Button } from "@/components/ui/button";
 import {
   Card,
+  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 interface Video {
@@ -35,23 +37,29 @@ function IndexesPage() {
   return (
     <div>
       <h1 className="text-xl font-extrabold tracking-tight mb-4">Projects</h1>
-      <div className="flex gap-4">
+      <div className="grid grid-cols-4 gap-4">
+        <Card className="flex items-center justify-center">
+          <CardContent className="text-slate-400 select-none text-center">
+            Create a project
+          </CardContent>
+        </Card>
         {videos.map((video) => (
-          <Card key={video.id} className="max-w-[420px] overflow-hidden">
-            <div className="pointer-events-none">
-              <YouTubeEmbed url={`${video.url}?controls=0`} />
-            </div>
-            <CardHeader>
-              <CardTitle className="line-clamp-1 leading-8">
-                {video.title}
-              </CardTitle>
-              <CardDescription className="line-clamp-3">
-                {video.description}
-              </CardDescription>
-            </CardHeader>
+          <Card key={video.id} className="overflow-hidden" asChild>
+            <Link href={`/indexes/${video.id}`}>
+              <div className="pointer-events-none">
+                <YouTubeEmbed url={`${video.url}?controls=0`} />
+              </div>
+              <CardHeader>
+                <CardTitle className="line-clamp-1 leading-8">
+                  {video.title}
+                </CardTitle>
+                <CardDescription className="line-clamp-2">
+                  {video.description}
+                </CardDescription>
+              </CardHeader>
+            </Link>
           </Card>
         ))}
-        <Button>Create</Button>
       </div>
     </div>
   );
